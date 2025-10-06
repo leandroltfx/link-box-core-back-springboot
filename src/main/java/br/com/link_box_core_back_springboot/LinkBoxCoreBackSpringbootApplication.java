5014946e6870f8884fe485adba89ec6d65ce3e37
leandroltfx <leandroltfbr@hotmail.com>
2025-10-06 07:55:01 -0300

    inicia o projeto

---
+++ src/main/java/br/com/link_box_core_back_springboot/LinkBoxCoreBackSpringbootApplication.java
@@ -0,0 +1,13 @@
+package br.com.link_box_core_back_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkBoxCoreBackSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkBoxCoreBackSpringbootApplication.class, args);
+	}
+
+}
