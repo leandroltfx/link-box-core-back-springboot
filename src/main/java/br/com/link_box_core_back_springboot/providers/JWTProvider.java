beaf50d2eeb78fafe391196a87f90dddc50c63af
leandroltfx <leandroltfbr@hotmail.com>
2025-10-10 22:43:27 -0300

    adiciona anotação de serviço à classe JWTProvider

--- src/main/java/br/com/link_box_core_back_springboot/providers/JWTProvider.java
+++ src/main/java/br/com/link_box_core_back_springboot/providers/JWTProvider.java
@@ -3,11 +3,13 @@ package br.com.link_box_core_back_springboot.providers;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.stereotype.Service;
 
 import java.time.Duration;
 import java.time.Instant;
 import java.util.UUID;
 
+@Service
 public class JWTProvider {
 
     public String generateToken(UUID id) {
