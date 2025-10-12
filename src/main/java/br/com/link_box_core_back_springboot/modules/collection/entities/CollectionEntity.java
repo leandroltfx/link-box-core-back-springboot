15ebfa98b7f721927ede6a8522ea3bd53f4127f4
leandroltfx <leandroltfbr@hotmail.com>
2025-10-12 17:01:09 -0300

    altera nome de tabela de coleções

--- src/main/java/br/com/link_box_core_back_springboot/modules/collection/entities/CollectionEntity.java
+++ src/main/java/br/com/link_box_core_back_springboot/modules/collection/entities/CollectionEntity.java
@@ -11,7 +11,7 @@ import org.hibernate.annotations.CreationTimestamp;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-@Entity(name = "collection")
+@Entity(name = "collections")
 @Data
 @Builder
 @AllArgsConstructor
